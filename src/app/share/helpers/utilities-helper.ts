5811379d691e0c3c6f56dbf44bed9f114809801e
Le Hong Phuc <phucb1909971@student.ctu.edu.vn>
2023-12-13 02:12:37 +0700

    Enhenment 1
    change login page, add page name

--- src/app/share/helpers/utilities-helper.ts
+++ src/app/share/helpers/utilities-helper.ts
@@ -13,3 +13,25 @@ export function FormatDate(date: Date): string {
 export function FormatNumber(number: number): string {
   return formatNumber(number, 'en-US', '1.0-0');
 }
+
+export function StatusColor(status: string) {
+  let bg = 'background-color: ';
+  switch (status) {
+    case 'Done':
+      return bg + '#90EE90';
+    case 'Processing':
+      return bg + '#ADD8E6';
+    case 'In Review':
+      return bg + '#ADD8E6';
+    case 'Pending':
+      return bg + '#ADD8E6';
+    case 'Cancel by User':
+      return bg + '#ADD8E6';
+    case 'Rejected by Admin':
+      return bg + '#F08080';
+    case 'Closed':
+      return bg + '#F08080';
+    default:
+      return '';
+  }
+}
